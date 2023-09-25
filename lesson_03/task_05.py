4112764cb052bb2b059f712b5ac59b2a16b372f7
afansenkosergey <afansenko.se@gmail.com>
2023-09-25 13:34:36 +0300

    Made

--- lesson_03/task_05.py
+++ lesson_03/task_05.py
@@ -2,7 +2,7 @@ lis = [1, 2, 3]
 fou = lis.append('four')
 tw = lis[1] = 'two'
 print(lis)
-dobav = lis.append({5, 6})
-dobav_2 = lis[4] = {5, 6, 7}
-dobav_3 = lis.insert(2,(2.5, 2.6))
+add_1 = lis.append({5, 6})
+add_2 = lis[4] = {5, 6, 7}
+add_3 = lis.insert(2, (2.5, 2.6))
 print(lis)
