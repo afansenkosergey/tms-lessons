531be1be1cbd89f35af543d021a14e6beb05cfa0
afansenkosergey <afansenko.se@gmail.com>
2023-09-28 15:04:08 +0300

    Made

--- homework_04/task_02.py
+++ homework_04/task_02.py
@@ -1,5 +1,6 @@
 for i in range(0, 100):
-    print(i)
+
     answer = input('Should we break?: ')
     if answer == 'yes':
         break
+    print(i)
--- homework_04/task_03.py
+++ homework_04/task_03.py
@@ -1,11 +1,12 @@
-s = 0
-for i in range(0, 100):
-    s += i
-    print(i)
-    answer = input('Should we break?: ')
-    if answer == 'yes':
+for i in range(100):
+
+    while True:
+        answer = input('Should we break?: ')
+        if answer != "no" and answer != "yes":
+            print("Don't understand you")
+        else:
+            break
+
+    if answer == "yes":
         break
-    elif answer == 'no':
-        continue
-    else:
-        print("Don't understand you")
+    print(i)
