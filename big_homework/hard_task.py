9c57d177e1565092a37d41ecc27ce1845921467a
afansenkosergey <afansenko.se@gmail.com>
2023-10-30 15:03:51 +0300

    Changed hard_task.py

--- big_homework/hard_task.py
+++ big_homework/hard_task.py
@@ -3,10 +3,10 @@ def is_magic_square(matrix):
     sum_first_row = sum(matrix[0])
 
     ''''Список суммы строк'''
-    row_sums = [sum(row) for row in matrix]
+    row_sum = [sum(row) for row in matrix]
 
     '''Список суммы столбцов'''
-    col_sums = [sum(matrix[i][j] for i in range(n)) for j in range(n)]
+    col_sum = [sum(matrix[i][j] for i in range(n)) for j in range(n)]
 
     '''Сумма первой диагонали'''
     primary_diagonal_sum = sum(matrix[i][i] for i in range(n))
@@ -14,8 +14,8 @@ def is_magic_square(matrix):
     '''Сумма второй диагонали'''
     secondary_diagonal_sum = sum(matrix[i][n - i - 1] for i in range(n))
 
-    return all(
-        all_sum == sum_first_row for all_sum in row_sums + col_sums + [primary_diagonal_sum, secondary_diagonal_sum])
+    return all(all_sum == sum_first_row for all_sum in
+               row_sum + col_sum + [primary_diagonal_sum, secondary_diagonal_sum])
 
 
 input_size_matrix = int(input("Enter the size of the square matrix: "))
